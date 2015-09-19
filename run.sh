fc0ef43aa296aa032a42eb8f52cd8e6bec4b398e
Sean Macdonald <sean@crazyhorsecoding.com>
2015-09-19 19:38:12 -0400

    now seeding data on run, and provind an optional to shell to access MongoDB

--- run.sh
+++ run.sh
@@ -4,9 +4,10 @@ DIR="$(git rev-parse --show-toplevel)"
 source $DIR/vars.sh
 
 #   run all services, but wait 5 seconds in between to allow linking to work properly
+#   specify the services exactly, because it matters what order they are launched in
 
 echo "running services..."
-for service in $services; do
+for service in mongodb mongoclient nodejs; do
     bash $DIR/services/$service/run.sh && sleep 5;
 done
 
